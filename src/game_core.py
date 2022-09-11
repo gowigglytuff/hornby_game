6fb5176c01b207a7338a1dbc316bc2ec7e673b17
Jayden <jayden.grieve@gmail.com>
2022-09-11 13:38:38 -0700

    Fixed walking

--- src/game_core.py
+++ src/game_core.py
@@ -36,20 +36,20 @@ class GameController(object):
         self.five_second_timer_id = pygame.USEREVENT + 150
         self.ten_second_timer_id = pygame.USEREVENT + 151
         self.one_second_timer_id = pygame.USEREVENT + 152
-        self.quarter_second_timer_id = pygame.USEREVENT + 153
-        self.eighth_second_timer_id = pygame.USEREVENT + 154
-        self.timer_list = [self.one_second_timer_id, self.five_second_timer_id, self.ten_second_timer_id, self.quarter_second_timer_id, self.eighth_second_timer_id]
+        self.fifth_second_timer_id = pygame.USEREVENT + 153
+        self.twentieth_second_timer_id = pygame.USEREVENT + 154
+        self.timer_list = [self.one_second_timer_id, self.five_second_timer_id, self.ten_second_timer_id, self.fifth_second_timer_id, self.twentieth_second_timer_id]
 
     def parse_timer_event(self, event):
-        if event.type in [self.five_second_timer_id, self.ten_second_timer_id, self.one_second_timer_id, self.eighth_second_timer_id]:
+        if event.type in [self.five_second_timer_id, self.ten_second_timer_id, self.one_second_timer_id, self.twentieth_second_timer_id]:
             pass
 
     def initiate_timers(self):
-        eighth_second_timer = self.eighth_second_timer_id
-        pygame.time.set_timer(eighth_second_timer, 5)
+        twentieth_second_timer = self.twentieth_second_timer_id
+        pygame.time.set_timer(twentieth_second_timer, 7)
 
-        quarter_second_timer = self.quarter_second_timer_id
-        pygame.time.set_timer(quarter_second_timer, 20)
+        fifth_second_timer = self.fifth_second_timer_id
+        pygame.time.set_timer(fifth_second_timer, 20)
 
         one_second_timer = self.one_second_timer_id
         pygame.time.set_timer(one_second_timer, 1000)
@@ -129,11 +129,15 @@ class GameData(object):
         self.key_item_data_list = {}
         self.goal_data_list = {}
         self.outfit_data_list = {}
+        self.animation_list = {}
 
         self.spritesheet_list = {}
 
     def add_spritesheet(self, spritesheet_name, spritesheet_object):
         self.spritesheet_list[spritesheet_name] = spritesheet_object
+
+    def add_animation(self, animation_name, animation_object):
+        self.animation_list[animation_name] = animation_object
 
     def add_player_data(self, player_object):
         self.player_data = player_object
@@ -207,11 +211,13 @@ class GameView(object):
         self.game_data = game_data  # type: GameData
         self.game_controller = game_controller  # type: GameController
         self.clock = pygame.time.Clock()
-        self.resolution = (320, 320)
-        self.FPS = 30
-        self.square_size = [32, 32]
-        self.base_locator_x = (self.resolution[0] / 2) /self.square_size[0]
-        self.base_locator_y = (self.resolution[1] / 2) /self.square_size[1]
+        self.resolution = (312, 312)
+        self.FPS = 72
+        self.square_size = [24, 24]
+        self.base_locator_x = ((self.resolution[0] - self.square_size[0]) / 2)
+        self.base_locator_y = ((self.resolution[1] - self.square_size[1]) / 2)
+
+        self.camera = [0, 0]
         self.screen = pygame.display.set_mode(self.resolution)
         self.font = "assets/fonts/PressStart.ttf"
         self.active_keyboard_manager = InGameKeyboardManager(self)  # type: KeyboardManager
@@ -221,18 +227,29 @@ class GameView(object):
         self.fully_dark_hours = 4
         self.sky_change_degree = 15
 
-        self.tile_set = TileSet("assets/tile_set/tile_set_1.png", 32, 32, 10, 10).load_tile_images()
-        self.bg = TileMap("assets/tile_set/tile_map_1.csv", self.tile_set, 25).return_map()
+        self.tile_set = TileSet("assets/tile_set/tile_set_1.png", self.square_size[0], self.square_size[1], 10, 10).load_tile_images()
+        self.bg = TileMap("assets/tile_set/tile_map_1.csv", self.tile_set, self.square_size[0]).return_map()
 
         self.image = 1
-        self.test_y = 50-12.5
+        self.test_y = 0
+        self.test_x = 0
         self.test_switch = 1
 
+        self.direction = Direction.DOWN
+        self.image1 = 0
         self.image2 = 1
-        self.test_y2 = 75-10
-        self.test_switch2 = 1
-        self.character_test = Spritesheet("player_base_spritesheet", "assets/spritesheets/test_run2.png", 25, 35)
-        self.character_test2 = Spritesheet("player_base_spritesheet", "assets/spritesheets/test_run4.png", 25, 35)
+        self.offset_y = 36/2
+        self.test_y2 = 0
+        self.test_x2 = 0
+
+        self.test_switch2 = 0
+        self.character_test = Spritesheet("player_base_spritesheet", "assets/spritesheets/player1.png", 24, 36)
+        self.character_test2 = Spritesheet("player_base_spritesheet", "assets/spritesheets/player1.png", 24, 36)
+
+        self.character = PlayerImage(self, 5, 5)
+
+        self.key_down_queue = []
+
     def tick(self):
         self.clock.tick(self.FPS)
 
@@ -240,23 +257,19 @@ class GameView(object):
         self.active_keyboard_manager = active_manager
 
     def draw_tester(self, screen):
-        screen.blit(self.character_test.get_image(self.image, 0), [100-18, ((-(self.test_y2/32) + 1) * self.square_size[1]+self.test_y)])
-        screen.blit(self.character_test2.get_image(self.image2, 0), [100 + 50 - 18, 100])
+        screen.blit(self.character_test.get_image(self.image, 0), [24*4, ((-(self.camera[1]/self.square_size[1]) + 1) * self.square_size[1] + self.test_y)])
 
     def draw_player(self, screen):
-        player_state = self.game_controller.get_player_state()
-        self_x = (self.base_locator_x * self.square_size[0])
-        self_y = ((self.base_locator_y * self.square_size[1]) - self.game_data.player_data.image_offset_y)
-        screen.blit(self.game_data.spritesheet_list[player_state.spritesheet].get_image(player_state.cur_img[0], player_state.cur_img[1]), [self_x, self_y])
+        screen.blit(self.character.spritesheet.get_image(self.character.current_image_x, self.character.current_image_y), [self.character.image_x * self.square_size[0], self.character.image_y*self.square_size[1]-self.offset_y])
 
     def draw_bg(self):
-        bg_x = (((self.game_controller.game.game_state.camera[0]) + 1) * self.square_size[0])
-        bg_y = ((-(self.test_y2/32) + 1) * self.square_size[1])
+        bg_x = ((-(self.camera[0]/self.square_size[0])) * self.square_size[0])
+        bg_y = ((-(self.camera[1]/self.square_size[1])) * self.square_size[1])
         self.screen.blit(self.bg, (bg_x, bg_y))
 
     def draw_all(self):
         self.draw_bg()
-        #self.draw_player(self.screen)
+        self.draw_player(self.screen)
         self.draw_tester(self.screen)
 
     def parse_input_event(self, event):
@@ -264,61 +277,64 @@ class GameView(object):
             self.game_controller.close_game()
 
         elif event.type in [pygame.KEYDOWN, pygame.KEYUP]:
-            self.active_keyboard_manager.parse_key_input(event.type, event.key)
+            # self.active_keyboard_manager.parse_key_input(event.type, event.key)
+            if event.type == pygame.KEYDOWN:
+                if event.key in [pygame.K_DOWN, pygame.K_UP, pygame.K_RIGHT, pygame.K_LEFT]:
+                    self.key_down_queue = event.key
+
+            if event.type == pygame.KEYUP:
+                if event.key in [pygame.K_DOWN, pygame.K_UP, pygame.K_RIGHT, pygame.K_LEFT]:
+                    if self.key_down_queue == event.key:
+                        self.key_down_queue = []
 
         elif event.type in self.game_controller.timer_list:
             if event.type == self.game_controller.one_second_timer_id:
-                print("1 second)")
+                pass
 
-            if event.type == self.game_controller.quarter_second_timer_id:
-                if self.test_switch <= 40:
+            if event.type == self.game_controller.fifth_second_timer_id:
+                if self.test_switch <= 24:
                     change = 1
 
-                    if self.test_switch == 5:
+                    if self.test_switch == 2:
                         self.image = 3
 
-                    elif self.test_switch == 15:
+                    elif self.test_switch == 8:
                         self.image = 0
 
-                    elif self.test_switch == 25:
+                    elif self.test_switch == 14:
                         self.image = 1
 
-                    elif self.test_switch == 35:
+                    elif self.test_switch == 20:
                         self.image = 0
 
                     # if ((self.test_switch > 7) and (self.test_switch < 20) or ((self.test_switch > 27) and (self.test_switch < 40))):
                     self.test_y += change
                     self.test_switch += 1
 
-                elif self.test_switch == 41:
+                elif self.test_switch == 25:
                     self.test_switch = 1
                 else:
                     self.test_switch += 1
                 if self.test_y >= 700:
-                    self.test_y = 25-10
+                    self.test_y = 24-10
 
-            if event.type == self.game_controller.eighth_second_timer_id:
-                change = 1
-                if self.test_switch2 <= 50:
-                    if self.test_switch2 == 5:
-                        self.image2 = 3
-                        self.test_y2 += change
+            if event.type == self.game_controller.twentieth_second_timer_id:
+                if not self.character.currently_animating:
+                    if self.key_down_queue == pygame.K_DOWN:
+                        self.character.initiate_animation("walk_front")
 
-                    elif self.test_switch2 == 30:
-                        self.image2 = 1
-                        self.test_y2 += change
+                    elif self.key_down_queue == pygame.K_UP:
+                        self.character.initiate_animation("walk_up")
 
-                    else:
-                        self.test_y2 += change
+                    elif self.key_down_queue == pygame.K_RIGHT:
+                        self.character.initiate_animation("walk_right")
 
-                    self.test_switch2 += 1
+                    elif self.key_down_queue == pygame.K_LEFT:
+                        self.character.initiate_animation("walk_left")
 
-                elif self.test_switch2 == 51:
-                    self.test_switch2 = 1
-                    self.test_y2 += change
+                if self.character.currently_animating:
+                    self.character.perform_animation()
 
-                if self.test_y2 >= 700:
-                    self.test_y2 = 25-10
 
 class InventoryManager(object):
     def __init__(self):
@@ -625,3 +641,141 @@ class InMenuKeyboardManager(KeyboardManager):
 
     def key_caps_released(self):
         pass
+
+
+class PlayerImage:
+    def __init__(self, game_view, image_x, image_y):
+        self.game_view = game_view  # type: GameView
+        self.spritesheet = Spritesheet("player_base_spritesheet", "assets/spritesheets/player.png", 24, 36)
+        self.current_image_x = 0
+        self.current_image_y = 0
+        self.image_x = image_x
+        self.image_y = image_y
+        self.animation_list = {"walk_front": WalkAnimation(self, Direction.DOWN),
+                               "walk_left": WalkAnimation(self, Direction.LEFT),
+                               "walk_right": WalkAnimation(self, Direction.RIGHT),
+                               "walk_up": WalkAnimation(self, Direction.UP)}
+        self.animation_frame = 0
+        self.currently_animating = False
+        self.current_animation = None
+
+    def initiate_animation(self, animation_name):
+        self.current_animation = animation_name
+        self.currently_animating = True
+
+    def perform_animation(self):
+        animation_result = (self.animation_list[self.current_animation].animate())
+        self.current_image_x = animation_result[2]
+        self.current_image_y = animation_result[3]
+        self.game_view.camera[0] += animation_result[0]
+        self.game_view.camera[1] += animation_result[1]
+        complete = animation_result[4]
+        if complete:
+            self.currently_animating = False
+            self.current_animation = None
+
+class Animation(object):
+    def __init__(self, game_view, direction):
+        self.game_view = game_view
+        self.total_distance = 0
+        self.direction = direction
+        self.current_frame = 0
+        self.frequency = 0
+
+        self.complete = False
+
+        self.y_change = 0
+        self.x_change = 0
+        self.current_image_x = 0
+        self.current_image_y = 0
+        self.vector = 1
+        self.changing_variable = self.x_change
+        if self.direction == Direction.UP:
+            self.vector = -1
+            self.changing_variable = self.y_change
+            self.current_image_y = 1
+
+        elif self.direction == Direction.LEFT:
+            self.vector = -1
+            self.current_image_y = 2
+
+        elif self.direction == Direction.DOWN:
+            self.current_image_y = 0
+            self.changing_variable = self.y_change
+
+        elif self.direction == Direction.RIGHT:
+            self.current_image_y = 3
+
+    def animate(self):
+        pass
+
+class WalkAnimation(Animation):
+    def __init__(self, game_view, direction):
+        super().__init__(game_view, direction)
+        self.foot = "left"
+        self.current_image_x = 0
+        self.current_image_y = 0
+
+        self.x_vector = 0
+        self.y_vector = 0
+
+        self.changing_variable = self.x_change
+        self.set_directions()
+
+    def set_directions(self):
+        if self.direction == Direction.UP:
+            self.y_vector = -1
+            self.x_vector = 0
+            self.changing_variable = self.y_change
+            self.current_image_y = 1
+        elif self.direction == Direction.LEFT:
+            self.y_vector = 0
+            self.x_vector = -1
+            self.current_image_y = 3
+        elif self.direction == Direction.DOWN:
+            self.y_vector = 1
+            self.x_vector = 0
+            self.current_image_y = 0
+            self.changing_variable = self.y_change
+        elif self.direction == Direction.RIGHT:
+            self.y_vector = 0
+            self.x_vector = 1
+            self.current_image_y = 2
+
+    def animate(self):
+        if self.current_frame <= 23:
+            if self.current_frame == 0:
+                if self.foot == "left":
+                    self.current_image_x = 3
+                elif self.foot == "right":
+                    self.current_image_x = 1
+
+            elif self.current_frame == 23:
+                self.current_frame = 0
+                self.current_image_x = 0
+                self.complete = True
+
+            self.current_frame += 1
+        return self.result()
+
+    def reset(self):
+        self.current_frame = 0
+        self.switch_foot()
+        self.complete = False
+
+    def switch_foot(self):
+        if self.foot == "left":
+            self.foot = "right"
+        elif self.foot == "right":
+            self.foot = "left"
+
+    def result(self):
+        y_change = self.y_vector
+        x_change = self.x_vector
+        sheet_x = self.current_image_x
+        sheet_y = self.current_image_y
+        complete = self.complete
+        if self.complete:
+            self.reset()
+
+        return x_change, y_change, sheet_x, sheet_y, complete
